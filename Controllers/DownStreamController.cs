76ae15f81679389b01c0d0b9ddabd0e16d5bcb2b
Tarun Narang <Tarun.Narang@bain.com>
2021-04-05 15:09:02 +0530

    first coimmit

---
+++ Controllers/DownStreamController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TodoListAPI.Controllers
+{
+    [Authorize]
+    [AuthorizeForScopes(ScopeKeySection = "MyAPI2:Scopes")]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DownStreamController : ControllerBase
+    {
+        private IDownstreamWebApi _downstreamWebApi;
+
+        public DownStreamController(IDownstreamWebApi downstreamWebApi)
+        {
+            _downstreamWebApi = downstreamWebApi;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Details()
+        {
+            HttpResponseMessage value = null;
+            //try
+            //{
+                 value = await _downstreamWebApi.CallWebApiForAppAsync(
+                    "MyAPI2",
+                    options =>
+                    {
+                        //options.BaseUrl = "https://localhost:44352/";
+                        options.HttpMethod = HttpMethod.Get;
+                        options.RelativePath = $"api/todolist2/getAllTasks2";
+                    });
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.StackTrace);
+            //}
+            return Ok(value);
+
+        }
+
+
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TodoListAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetAPI2")]
+        public async Task<string> GetAPI2()
+        {
+            HttpClient client = new HttpClient();
+            var defaultRequestHeaders = client.DefaultRequestHeaders;
+            //string webApiUrl = "https://localhost:44352/api/todolist2/getAllTasks2";
+            string webApiUrl = "https://localhost:44352/WeatherForecast";
+            
+            HttpResponseMessage response = await client.GetAsync(webApiUrl);
+            string json = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("JSON = " + json);
+
+            return json;
+        }
+
+    }
+}
